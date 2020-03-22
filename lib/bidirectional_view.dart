6a93ffe721a0c121d46af8ea9071586b5465b4d2
isaac@zfamily.org <isaac@zfamily.org>
2020-03-22 09:15:01 -0600

    working version, always scales to global center (need to find inverse function)

--- lib/bidirectional_view.dart
+++ lib/bidirectional_view.dart
@@ -13,11 +13,11 @@ class BiDirectionalLayout extends ChangeNotifier {
   ///
   /// This function returns each [Widget] wrapped with a
   /// [LayoutId] so that they can be used by the delegate.
-  List<LayoutId> widgets(Matrix4 transform) => children
+  List<LayoutId> widgets(Offset origin, double scale) => children
       .map((w) => LayoutId(
           id: w.key,
           child: Transform(
-            transform: transform,
+            transform: _transform(w, origin, scale),
             child: GestureDetector(
                 child: w.child,
                 onPanUpdate: (details) {
@@ -26,6 +26,10 @@ class BiDirectionalLayout extends ChangeNotifier {
                 }),
           )))
       .toList();
+
+  Matrix4 _transform(BiWrapper w, Offset origin, double scale) {
+    return Matrix4.identity()..scale(scale, scale);
+  }
 }
 
 class BiDirectionalView extends StatefulWidget {
@@ -43,19 +47,22 @@ class BiDirectionalView extends StatefulWidget {
 
 class _BiDirectionalViewState extends State<BiDirectionalView> {
   double scale;
-  Matrix4 matrix;
+  Offset origin;
 
   _BiDirectionalViewState();
 
   @override
   initState() {
     scale = 1.0;
-    matrix = Matrix4.identity();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    origin = origin ??
+        Offset(MediaQuery.of(context).size.width / 2,
+            MediaQuery.of(context).size.height / 2);
+
     BiDirectionalViewLayoutDelegate delegate =
         BiDirectionalViewLayoutDelegate(this, layout: widget.layout);
 
@@ -63,9 +70,7 @@ class _BiDirectionalViewState extends State<BiDirectionalView> {
       children: <Widget>[
         GestureDetector(
           behavior: HitTestBehavior.translucent,
-          onPanUpdate: (details) {
-            setState(() => _updateMatrixTransform(details.delta));
-          },
+          onPanUpdate: (details) => _updateMatrixTransform(details.delta),
           child: Container(
             width: double.infinity,
             height: double.infinity,
@@ -74,7 +79,7 @@ class _BiDirectionalViewState extends State<BiDirectionalView> {
         ),
         CustomMultiChildLayout(
           delegate: delegate,
-          children: delegate.layout.widgets(matrix),
+          children: delegate.layout.widgets(origin, scale),
         ),
         Align(
             alignment: Alignment.bottomRight,
@@ -99,9 +104,8 @@ class _BiDirectionalViewState extends State<BiDirectionalView> {
                           max: 3.0,
                           divisions: 50,
                           label: scale.toStringAsFixed(2),
-                          onChanged: (double value) {
-                            setState(() => _updateMatrixScale(value));
-                          },
+                          onChanged: (double value) =>
+                              _updateMatrixScale(value),
                         ),
                       ),
                     ),
@@ -117,60 +121,58 @@ class _BiDirectionalViewState extends State<BiDirectionalView> {
   }
 
   void _updateMatrixScale(double newScale) {
-    scale = newScale;
-    matrix.setDiagonal(math.Vector4(scale, scale, 1.0, 1.0));
+    setState(() {
+      scale = newScale;
+    });
   }
 
   void _updateMatrixTransform(Offset delta) {
-    /// This converts delta pixels to delta global (2D) space
-    ///
-    /// This was found with the following:
-    /// ```
-    /// double c = 65.0;
-    /// math.Vector4 vec = math.Vector4((delta.dx) * (1/(scale*c)), (delta.dy) * (1/(scale*c)), 0.0, 0.0);
-    /// print ('${delta.dx}: ($scale, ${(1/(scale*c))})');
-    /// ```
-    /// to find different `c` values given `scale`.
-    ///
-    /// Thus, f(x) returns the function that fits the scale to the transformation.
-    /// This is used to keep the distance the users mouse travels consistent with the view.
-    ///
-    /// The distance travelled as `scale` changes is non-linear and this is function I found
-    /// after trying many values (recorded here) and using regression to find a best fit line.
-    ///
-    /// Data (copy paste into excel, comma delimited):
-    /// Note that x is `scale`, y is (1/(scale*c))
-    /// where I found c manually to make the drag feel natural (keep mouse position consistent)
-    ///
-    /// x,y
-    /// 0.015625,0.984615385
-    /// 0.03125,0.96969697
-    /// 0.0625,0.941176471
-    /// 0.125,0.888888889
-    /// 0.25,0.8
-    /// 0.5,0.666666667
-    /// 1,0.5
-    /// 2,0.333333333
-    /// 3,0.245098039
-    /// 4,0.2
-    ///
-    /// Using excel, the best regression was polynomial (n=5, R^2=1):
-    /// y = -0.006x^5 + 0.0686x^4 - 0.3039x^3 + 0.6859x^2 - 0.9418^x + 0.998
-    double f(double x) => (-0.006 * pow(x, 5) +
-            0.0686 * pow(x, 4) -
-            0.3039 * pow(x, 3) +
-            0.6859 * pow(x, 2) -
-            0.9418 * pow(x, 1) +
-            0.998)
-        .abs();
-
-    double y = f(scale);
-    math.Vector4 vec = math.Vector4(delta.dx * y, delta.dy * y, 0.0, 0.0);
-
-    Matrix4 toAdd = Matrix4.zero();
-    toAdd.setColumn(3, vec);
-    matrix = matrix + toAdd;
+    setState(() {
+      origin += delta * f(scale);
+    });
   }
+
+  /// This converts delta pixels to delta global (2D) space
+  ///
+  /// This was found with the following:
+  /// ```
+  /// double c = 65.0;
+  /// math.Vector4 vec = math.Vector4((delta.dx) * (1/(scale*c)), (delta.dy) * (1/(scale*c)), 0.0, 0.0);
+  /// print ('${delta.dx}: ($scale, ${(1/(scale*c))})');
+  /// ```
+  /// to find different `c` values given `scale`.
+  ///
+  /// Thus, f(x) returns the function that fits the scale to the transformation.
+  /// This is used to keep the distance the users mouse travels consistent with the view.
+  ///
+  /// The distance travelled as `scale` changes is non-linear and this is function I found
+  /// after trying many values (recorded here) and using regression to find a best fit line.
+  ///
+  /// Data (copy paste into excel, comma delimited):
+  /// Note that x is `scale`, y is (1/(scale*c))
+  /// where I found c manually to make the drag feel natural (keep mouse position consistent)
+  ///
+  /// x,y
+  /// 0.015625,0.984615385
+  /// 0.03125,0.96969697
+  /// 0.0625,0.941176471
+  /// 0.125,0.888888889
+  /// 0.25,0.8
+  /// 0.5,0.666666667
+  /// 1,0.5
+  /// 2,0.333333333
+  /// 3,0.245098039
+  /// 4,0.2
+  ///
+  /// Using excel, the best regression was polynomial (n=5, R^2=1):
+  /// y = -0.006x^5 + 0.0686x^4 - 0.3039x^3 + 0.6859x^2 - 0.9418^x + 0.998
+  double f(double x) => (-0.006 * pow(x, 5) +
+          0.0686 * pow(x, 4) -
+          0.3039 * pow(x, 3) +
+          0.6859 * pow(x, 2) -
+          0.9418 * pow(x, 1) +
+          0.998)
+      .abs();
 }
 
 class BiDirectionalViewLayoutDelegate extends MultiChildLayoutDelegate {
@@ -181,12 +183,12 @@ class BiDirectionalViewLayoutDelegate extends MultiChildLayoutDelegate {
 
   @override
   void performLayout(Size size) {
-    List<double> vec = [0, 0, 0];
-    viewState?.matrix?.getTranslation()?.copyIntoArray(vec);
+    // Offset offset = Offset(size.width/2, size.height/2);
 
     layout.children.forEach((wrapper) {
       layoutChild(wrapper.key, BoxConstraints.loose(size));
-      positionChild(wrapper.key, Offset(vec[0], vec[1]) + wrapper.worldPos);
+      positionChild(
+          wrapper.key, viewState.origin + (wrapper.worldPos * viewState.scale));
     });
   }
 
